127355b3b58da77c00364f98335b297d780efcfc
Silent <Nikich213@gmail.com>
2021-04-19 17:43:12 +0500

    Change regex pattern from PhoneAttribute

--- ExchangeOfCurrencies/ClientModel/Validation/PhoneAttribute.cs
+++ ExchangeOfCurrencies/ClientModel/Validation/PhoneAttribute.cs
@@ -12,7 +12,7 @@ namespace ExchangeOfCurrencies.ClientModel.Validation
         public override bool IsValid(object value)
         {
             string phone = value as string;
-            return Regex.IsMatch(phone, @"\+7 \(\d{3}\) \d{3}-\d{2}-\d{2}"); // +7 (123) 456-78-90
+            return Regex.IsMatch(phone, @"\+7 \d{3} \d{3}\d{2}\d{2}"); // +7 123 4567890
         }
     }
 }

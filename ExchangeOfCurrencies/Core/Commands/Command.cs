8f113b9446beb61f67573877b33a5615cdc45c96
SilentHill <Nikich213@gmail.com>
2022-03-25 13:22:41 +0500

    Added mvvm pattern.

---
+++ ExchangeOfCurrencies/Core/Commands/Command.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace ExchangeOfCurrencies.Core.Commands;
+
+internal class Command : ICommand
+{
+    private readonly Action<object?> _execute;
+    private readonly Func<object?, bool>? _canExecute;
+
+
+    public Command(Action<object?> execute, Func<object?, bool>? canExecute)
+    {
+        _execute = execute;
+        _canExecute = canExecute;
+    }
+
+
+    public event EventHandler? CanExecuteChanged
+    {
+        add { CommandManager.RequerySuggested += value; }
+        remove { CommandManager.RequerySuggested -= value; }
+    }
+
+
+    public bool CanExecute(object? parameter)
+    {
+        return _canExecute is null || _canExecute(parameter);
+    }
+
+    public void Execute(object? parameter)
+    {
+        _execute(parameter);
+    }
+}

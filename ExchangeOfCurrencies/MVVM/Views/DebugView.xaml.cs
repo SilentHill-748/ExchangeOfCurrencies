89f6313749384c5eb135cccf5597225d43291279
SilentHill <Nikich213@gmail.com>
2022-03-29 17:09:56 +0500

    Changes in colors of the dark theme. Added DebugView for debug all views.

---
+++ ExchangeOfCurrencies/MVVM/Views/DebugView.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System.Windows;
+
+namespace ExchangeOfCurrencies.MVVM.Views
+{
+    public partial class DebugView : Window
+    {
+        public DebugView()
+        {
+            InitializeComponent();
+        }
+
+        private void ShowAutorizViewBtn_Click(object sender, RoutedEventArgs e)
+        {
+            new AutorizationView().Show();
+        }
+
+        private void ShowRegViewBtn_Click(object sender, RoutedEventArgs e)
+        {
+            new RegistrationView().Show();
+        }
+
+        private void ShowMainViewBtn_Click(object sender, RoutedEventArgs e)
+        {
+            new MainWindow().Show();
+        }
+    }
+}

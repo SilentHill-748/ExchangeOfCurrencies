0e92f345c5a509e5f865057ff0dbccb67a6656df
SilentHill <Nikich213@gmail.com>
2022-04-01 13:14:26 +0500

    Added page navigation and do some refactoring.

--- ExchangeOfCurrencies/MVVM/ViewModels/MainViewModel.cs
+++ ExchangeOfCurrencies/MVVM/ViewModels/MainViewModel.cs
@@ -3,14 +3,56 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Controls;
+
+using ExchangeOfCurrencies.Pages;
+using ExchangeOfCurrencies.Services;
+using ExchangeOfCurrencies.Core;
+using ExchangeOfCurrencies.Core.Commands;
 
 namespace ExchangeOfCurrencies.MVVM.ViewModels
 {
-    internal class MainViewModel
+    internal class MainViewModel : ObservableObject
     {
+        private readonly NavigationService _navigationService;
+        private Page _page;
+
+
         public MainViewModel()
         {
+            _navigationService = NavigationService.Instance;
 
+            _navigateToCommand = new Command(OnNavigateTo);
+            _page = new AccountInfo();
+        }
+
+
+        public Page Page
+        {
+            get => _page;
+            set
+            {
+                _page = value;
+                OnPropertyChanged(nameof(Page));
+            }
+        }
+
+
+        #region Commands
+        private Command _navigateToCommand;
+        public Command NavigateToCommand
+            => _navigateToCommand;
+        #endregion
+
+
+        private void OnNavigateTo(object? commandParameter)
+        {
+            if (commandParameter is null)
+            {
+                return;
+            }
+            else
+                Page = _navigationService.NavigateTo((Type)commandParameter);
         }
     }
 }

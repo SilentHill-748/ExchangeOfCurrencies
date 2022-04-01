255b13c3f96ed88005ed1fd0137447bfd741e865
SilentHill <Nikich213@gmail.com>
2022-04-01 19:55:45 +0500

    Added MessageView for display information for user.

---
+++ ExchangeOfCurrencies/Core/MessageViewButtons.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExchangeOfCurrencies.Core
+{
+    public enum MessageViewButtons
+    {
+        Ok,
+        Cancel,
+        OkCancel,
+        YesNo
+    }
+}
---
+++ ExchangeOfCurrencies/MVVM/Views/MessageView.xaml.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+
+using ExchangeOfCurrencies.Core;
+
+namespace ExchangeOfCurrencies.MVVM.Views
+{
+    public partial class MessageView : Window
+    {
+        private MessageViewButtons _messageViewButtons;
+
+        public MessageView()
+        {
+            InitializeComponent();
+        }
+
+        public MessageView(string title, string message, MessageViewButtons buttons) 
+            : this()
+        {
+            Title = title;
+            Message = message;
+
+            MessageViewButtonsChanged += MessageView_MessageViewButtonsChanged;
+            Buttons = buttons;
+
+        }
+
+
+        public event Action? MessageViewButtonsChanged;
+
+
+        public string Message { get; set; }
+
+        public MessageViewButtons Buttons
+        {
+            get => _messageViewButtons;
+            set
+            {
+                _messageViewButtons = value;
+                MessageViewButtonsChanged?.Invoke();
+            }
+        }
+
+
+        private void MessageView_MessageViewButtonsChanged()
+        {
+            switch (Buttons)
+            {
+                case MessageViewButtons.Ok:
+                    ChangeButton(BtnOne, "Ok");
+                    break;
+                case MessageViewButtons.Cancel:
+                    ChangeButton(BtnOne, "Cancel");
+                    break;
+                case MessageViewButtons.OkCancel:
+                    ChangeButton(BtnTwo, "Отмена");
+                    break;
+                default:
+                    ChangeButton(BtnOne, "Да");
+                    ChangeButton(BtnTwo, "Нет");
+                    break;
+            }
+        }
+
+        private static void ChangeButton(Button button, string content, Visibility value = Visibility.Visible)
+        {
+            button.Content = content;
+            button.Visibility = value;
+        }
+
+        private void BtnOne_Click(object sender, RoutedEventArgs e)
+        {
+            this.DialogResult = true;
+            this.Close();
+        }
+
+        private void BtnTwo_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+    }
+}

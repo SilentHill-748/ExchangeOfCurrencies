01e53b6d34f86b37e765a828e6502f5cd51e1591
SilentHill <Nikich213@gmail.com>
2022-04-05 09:32:14 +0500

    Added remaining repositories.

---
+++ ExchangeOfCurrencies.Data/Repositories/CredentialsRepository.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+using ExchangeOfCurrencies.Data.Entities;
+
+namespace ExchangeOfCurrencies.Data.Repositories
+{
+    public class CredentialsRepository : Repository<Credentials>
+    {
+        public CredentialsRepository(DbContext dbContext)
+            : base(dbContext)
+        {
+
+        }
+    }
+}
---
+++ ExchangeOfCurrencies.Data/Repositories/CurrencyRateRepository.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+using ExchangeOfCurrencies.Data.Entities;
+
+namespace ExchangeOfCurrencies.Data.Repositories
+{
+    public class CurrencyRateRepository : Repository<CurrencyRate>
+    {
+        public CurrencyRateRepository(DbContext dbContext)
+            : base(dbContext)
+        {
+
+        }
+    }
+}
